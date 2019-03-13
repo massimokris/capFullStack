896d6c68ad17acb8921d2f0bb2cb0d1456d8487b
your name <you@example.com>
2019-03-13 14:33:12 -0300

    listo el ejercicio 3 de js

--- TGIFwebapp/salajuegos/main.js
+++ TGIFwebapp/salajuegos/main.js
@@ -54,7 +54,7 @@ var nombres = ["pedro", "jose", "manuel", "humberto", "rodrigo", "caro", "massim
 // namevarible.length muestra la cantidad de elementos
 // namevarible.push (valor) agrega un elemento al final de la matriz
 // namevarible.indexOf (valor) para vver igualdad de algun elemeto
-// namevarible.join (separado ejem ", ") miestra todos los elementos e cadena
+// namevarible.join (separado ejem ", ") muestra todos los elementos e cadena
 // namevarible.sort(); ordena la matriz
 
 nombres.length
@@ -100,18 +100,196 @@ for(contadorFor=0;contadorFor<nombres.length;contadorFor++){
 
 //terminamos bucle en array
 
-//comenzamos funciones de array
+//comenzamos funciones que utilizan array
 
 //sintaxis function nombrearray comparacion(a,b) { return (lo que quieras que devuelva) }
 
 //sintaxis b var array = (a,b) => (lo que quieras que regrese);
 
-var numeros = [1,2,3,4,5,6,7,8,9,10];
+//utilizamos una funcion en la cual recorremos un array con una iteracion para encontrar el numero menor
 
-function numeros.sort (a,b) {
+var contadorFuncion;
+var numeros = [2,3,5,6,9,10,4,7,8,1];
+var numeroMenor;
+
+contadorFuncion=0;
+
+function menor(numeros){
     
-    console.log(numeros [0]);
+    
+    while(contadorFuncion<numeros.length){
+        
+        if(contadorFuncion==0){
+            
+            numeroMenor=numeros[contadorFuncion];
+        }else{
+            
+            if(numeros[contadorFuncion]<numeroMenor){
+                numeroMenor=numeros[contadorFuncion];
+            } 
+        }
+        
+        contadorFuncion++  
+    }
+    
+return (numeroMenor);
+
 }
+
+console.log(menor(numeros));
+
+//terminamos el ejercicio anterior
+
+//utilizamos una funcion en la cual recorremos un array con una iteracion para encontrar el numero mayor
+
+var contadorMayor;
+var numerosMayor = [2,3,5,6,8,9,1,4,7,10];
+var numeroMayor;
+
+contadorMayor=0;
+
+function mayor(numerosMayor){
+    
+    while(contadorMayor<numerosMayor.length){
+        
+        if(contadorMayor==0){
+            
+            numeroMayor=numerosMayor[contadorMayor];
+        }else{
+            
+            if(numerosMayor[contadorMayor]>numeroMayor){
+                
+               numeroMayor=numerosMayor[contadorMayor]; 
+            }
+        }
+        
+        contadorMayor++
+    }
+    
+return (numeroMayor);
+}
+
+console.log(mayor(numerosMayor));
+
+//terminamos el ejercicio anterior
+
+//realizamos una funcion que recibe 2 parametros un array y un indice, y mostramos por consola el elemento en la posicio dada en el indice
+
+var array = [2,35,32,28,5,8,19,22,30,15,9];
+var posicion;
+
+function devolverparametro (array, posicion){
+    
+    posicion=prompt("Ingrese el numero de la posicion que desea ver");
+    
+    return (array[posicion]);
+}
+
+console.log(devolverparametro(array,posicion));
+
+//terminamos el ejercicio anterior
+
+//utilizamos una funcion en la cual recorremos un array y mostramos los datos iguales
+
+var numerosIguales = [3,6,3,6,1,1,5,4,8,9];
+
+function numeroIgual (array){
+    
+    var contadorIguales;
+    contadorIguales=0;
+    var iguales = [];
+    
+    while(contadorIguales<array.length){
+        
+       if(array.indexOf(array[contadorIguales]) != contadorIguales && iguales.indexOf(array[contadorIguales]) == -1){
+           
+           iguales.push(array[contadorIguales])
+       }
+       
+        contadorIguales++
+    }
+    
+    return iguales;
+}
+
+console.log(numeroIgual(numerosIguales));
+
+//aca intente hacer el ejercicio anterior con for pero no me salio espero comentarios para intentar terminarlo
+var numerosIguales2 = [3,6,3,6,1,1,5,4,8,9];
+
+function numeroIgual2 (array){
+    
+    var contadorIguales2;
+    var comparadorIguales;
+    var iguales2 = [];
+    
+    for(contadorIguales2=0;contadorIguales2<array.length;contadorIguales2++){
+        
+        for(comparadorIguales=0;comparadorIguales<array.length;comparadorIguales++){
+            
+            if(array[contadorIguales2] == array[comparadorIguales]){
+                
+                iguales2.push(array[comparadorIguales]);
+            }
+        }
+       
+    }
+    
+    return iguales2;
+}
+
+console.log(numeroIgual2(numerosIguales2));
+
+
+//terminamos el ejercicio anterior
+
+//con una funcion unimos todos los elementos de un array
+
+var mycolor = ["red", "green", "white", "black"];
+
+function ordenar (array){
+    
+    
+    return array.join(",");
+}
+
+console.log(ordenar(mycolor));
+
+//terminamos el ejercicio anterior
+
+
+//comenzamos string functions
+
+//metodos:
+//.legth para obtener la longitud
+//bigString.indexOf(searchString) encontrar una cadena dentro de una cadena mas grande si no la encuentra devuelve -1 
+//.startsWith(subString) para saber si un string comienza con un substring particular, esto devuelve verdadero o falso.
+//.endsWith lo mismo pero al revez
+//.toLowerCase()cambia el string a minuscula
+//.toUpperCase()cambia el string a mayuscula
+//.trim()para deshacer cualquier espacio al principio o al final del string
+
+//tomar un numero y mostrarlo invertido
+
+
+
+function invertir(String){
+    
+    var numero;
+    
+    numero=prompt("Ingrese un numero");
+    
+    for(var i = String - 1; i>-1; i--){
+        
+        result += String[i];
+    }
+    
+    return(result);
+}
+
+console.log(result);
+
+
 
 
 
