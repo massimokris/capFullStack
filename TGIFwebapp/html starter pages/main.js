8728fbffd0722fa2871ea7ce137afd3f46aca3e9
your name <you@example.com>
2019-03-19 14:21:31 -0300

    listo todo el estilo con bootstrap

--- TGIFwebapp/html starter pages/main.js
+++ TGIFwebapp/html starter pages/main.js
@@ -1,91 +1,70 @@
-document.getElementById("senate-data").innerHTML = JSON.stringify(data,null,2);
-
-//console.log(data.results[0].members);
-
-var senadores;
-var tablaSenadores;
-
-senadores = data.results[0].members;
-tablaSenadores =    "<tr>"+
-                    "<th>Full name</th>"+
-                    "<th>Party</th>"+
-                    "<th>State</th>"+
-                    "<th>Seniority</th>"+
-                    "<th>Percentage of votes</th>"+
-                    "</tr>"+ 
-                    "<tr>";
-
-console.log(senadores.length);
-
-for(var i=0;i<senadores.length;i++){
+function tabla (){
     
-    if(senadores[i].middle_name == null){
-        
-        tablaSenadores +=
-        "<tr>" +
-        " <td><a href="+senadores[i].url+">"+senadores[i].last_name+" "+senadores[i].first_name+"</a></td>"+ 
-        "<td>"+senadores[i].party+"</td>"+
-        "<td>"+senadores[i].state+"</td>"+
-        "<td>"+senadores[i].seniority+"</td>"+
-        "<td>"+senadores[i].votes_with_party_pct+"%</td>"+
-        "</tr>";
-    }else{
-        
-         tablaSenadores +=
-         "<td><a href="+senadores[i].url+">"+senadores[i].last_name+" "+senadores[i].first_name+" "+senadores[i].middle_name+"</a></td>"+
-         "<td>"+senadores[i].party+"</td>"+
-         "<td>"+senadores[i].state+"</td>"+
-         "<td>"+senadores[i].seniority+"</td>"+
-         "<td>"+senadores[i].votes_with_party_pct+"%</td>"+
-         "</tr>";
+    var miembros;
+    var tablaMiembros;
+
+    miembros = data.results[0].members;
+    tablaMiembros =    "<tr>"+
+                        "<th>Full name</th>"+
+                        "<th>Party</th>"+
+                        "<th>State</th>"+
+                        "<th>Years in Office</th>"+
+                        "<th>% Votes w/Party</th>"+
+                        "</tr>"+ 
+                        "<tr>";
+
+    //console.log(miembros.length);
+
+    for(var i=0;i<miembros.length;i++){
+
+        if(miembros[i].middle_name == null){
+
+            tablaMiembros +=
+            "<tr>" +
+            " <td><a href="+miembros[i].url+">"+miembros[i].first_name+" "+miembros[i].last_name+"</a></td>"+ 
+            "<td>"+miembros[i].party+"</td>"+
+            "<td>"+miembros[i].state+"</td>"+
+            "<td>"+miembros[i].seniority+"</td>"+
+            "<td>"+miembros[i].votes_with_party_pct+"%</td>"+
+            "</tr>";
+        }else{
+
+             tablaMiembros +=
+             "<td><a href="+miembros[i].url+">"+miembros[i].first_name+" "+miembros[i].middle_name+" "+miembros[i].last_name+"</a></td>"+
+             "<td>"+miembros[i].party+"</td>"+
+             "<td>"+miembros[i].state+"</td>"+
+             "<td>"+miembros[i].seniority+"</td>"+
+             "<td>"+miembros[i].votes_with_party_pct+"%</td>"+
+             "</tr>";
+        }
     }
+
+    /*console.log(tablaMiembros);
+    console.log(tablaMiembros.length);*/
+    
+    return tablaMiembros;
 }
 
-console.log(tablaSenadores);
-console.log(tablaSenadores.length);
+document.getElementById("members-data").innerHTML = tabla();
 
-document.getElementById("senate-data").innerHTML  = tablaSenadores;
-
-var camara;
-var tablaCamara;
-
-camara = data.results[0].members;
-tablaCamara =    "<tr>"+
-                    "<th>Full name</th>"+
-                    "<th>Party</th>"+
-                    "<th>State</th>"+
-                    "<th>Seniority</th>"+
-                    "<th>Percentage of votes</th>"+
-                    "</tr>"+ 
-                    "<tr>";
-
-console.log(camara.length);
-
-for(var i=0;i<camara.length;i++){
+function estados (){
     
-    if(camara[i].middle_name == null){
+    var estado;
+    var tablaEstados;
+    
+    estado = data.results[0].members;
+    tablaEstados= "<option>"+"all"+"</option>";
+    
+    for(var i=0;i<estado.length;i++){
         
-        tablaCamara +=
-        "<tr>" +
-        "<td>"+camara[i].last_name+" "+camara[i].first_name+"</td>"+ 
-        "<td>"+camara[i].party+"</td>"+
-        "<td>"+camara[i].state+"</td>"+
-        "<td>"+camara[i].seniority+"</td>"+
-        "<td>"+camara[i].votes_with_party_pct+"%</td>"+
-        "</tr>";
-    }else{
-        
-         tablaCamara +=
-         "<td>"+camara[i].last_name+" "+camara[i].first_name+" "+camara[i].middle_name+"</td>"+
-         "<td>"+camara[i].party+"</td>"+
-         "<td>"+camara[i].state+"</td>"+
-         "<td>"+camara[i].seniority+"</td>"+
-         "<td>"+camara[i].votes_with_party_pct+"%</td>"+
-         "</tr>";
+        tablaEstados += 
+            
+            "<option>"+estado[i].state+"</option>";
     }
+    
+    console.log(tablaEstados);
+    
+    return tablaEstados;
 }
 
-console.log(tablaCamara);
-console.log(tablaCamara.length);
-
-document.getElementById("house-data").innerHTML  = tablaCamara;
+document.getElementById("state-data").innerHTML = estados();
