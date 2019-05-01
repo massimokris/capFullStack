d22b0663ad6d7264234ba93c728f75c54d41cc53
Your Name <you@example.com>
2019-05-01 00:54:37 -0300

    app web lista

---
+++ NYSLmobile/main.js
@@ -0,0 +1,183 @@
+(function mostrarSchedule(){
+    
+    ocultar();
+    document.getElementById('topHeader').style.display="block";
+    document.getElementById('navBar').style.display="block";
+    document.getElementById('scoreboard').style.display="block";
+    document.getElementById('sliderImg').style.display="block";
+    document.getElementById('schedule_home').style.display="block";
+    
+    document.getElementById('footer').style.display="block";
+})();
+
+function mostrarSchedule2(){
+    
+    ocultar();
+    document.getElementById('topHeader').style.display="block";
+    document.getElementById('navBar').style.display="block";
+    document.getElementById('scoreboard').style.display="block";
+    document.getElementById('sliderImg').style.display="block";
+    document.getElementById('schedule_home').style.display="block";
+    
+    document.getElementById('footer').style.display="block";
+}
+
+function mostrarMaps(){
+    
+    ocultar();
+    document.getElementById('maps').style.display="block";
+    document.getElementById('topHeader').style.display="block";
+    document.getElementById('navBar').style.display="block";
+    document.getElementById('footer').style.display="block";
+}
+
+function mostrarContact(){
+    
+    ocultar();
+    document.getElementById('contact').style.display="block";
+     document.getElementById('topHeader').style.display="block";
+    document.getElementById('navBar').style.display="block";
+    document.getElementById('footer').style.display="block";
+}
+
+function mostrarU1(){
+    
+    ocultar();
+    document.getElementById('u1').style.display="block";
+     document.getElementById('topHeader').style.display="block";
+    document.getElementById('navBar').style.display="block";
+    document.getElementById('footer').style.display="block";
+}
+
+function mostrarU2(){
+    
+    ocultar();
+    document.getElementById('u2').style.display="block";
+     document.getElementById('topHeader').style.display="block";
+    document.getElementById('navBar').style.display="block";
+    document.getElementById('footer').style.display="block";
+}
+
+function mostrarU3(){
+    
+    ocultar();
+    document.getElementById('u3').style.display="block";
+     document.getElementById('topHeader').style.display="block";
+    document.getElementById('navBar').style.display="block";
+    document.getElementById('footer').style.display="block";
+}
+
+function mostrarU4(){
+    
+    ocultar();
+    document.getElementById('u4').style.display="block";
+     document.getElementById('topHeader').style.display="block";
+    document.getElementById('navBar').style.display="block";
+    document.getElementById('footer').style.display="block";
+}
+
+function mostrarU5(){
+    
+    ocultar();
+    document.getElementById('u5').style.display="block";
+     document.getElementById('topHeader').style.display="block";
+    document.getElementById('navBar').style.display="block";
+    document.getElementById('footer').style.display="block";
+}
+
+function mostrarU6(){
+    
+    ocultar();
+    document.getElementById('u6').style.display="block";
+     document.getElementById('topHeader').style.display="block";
+    document.getElementById('navBar').style.display="block";
+    document.getElementById('footer').style.display="block";
+}
+
+function mostrarCalvary(){
+    
+    ocultar();
+    document.getElementById('calvary').style.display="block";
+     document.getElementById('topHeader').style.display="block";
+    document.getElementById('navBar').style.display="block";
+    document.getElementById('footer').style.display="block";
+    document.getElementById('teams').style.display="block";
+}
+
+function mostrarEdmonton(){
+    
+    ocultar();
+    document.getElementById('edmonton').style.display="block";
+     document.getElementById('topHeader').style.display="block";
+    document.getElementById('navBar').style.display="block";
+    document.getElementById('footer').style.display="block";
+    document.getElementById('teams').style.display="block";
+}
+
+function mostrarForge(){
+    
+    ocultar();
+    document.getElementById('forge').style.display="block";
+     document.getElementById('topHeader').style.display="block";
+    document.getElementById('navBar').style.display="block";
+    document.getElementById('footer').style.display="block";
+    document.getElementById('teams').style.display="block";
+}
+
+function mostrarPacific(){
+    
+    ocultar();
+    document.getElementById('pacific').style.display="block";
+     document.getElementById('topHeader').style.display="block";
+    document.getElementById('navBar').style.display="block";
+    document.getElementById('footer').style.display="block";
+    document.getElementById('teams').style.display="block";
+}
+
+function mostrarYork(){
+    
+    ocultar();
+    document.getElementById('york').style.display="block";
+     document.getElementById('topHeader').style.display="block";
+    document.getElementById('navBar').style.display="block";
+    document.getElementById('footer').style.display="block";
+    document.getElementById('teams').style.display="block";
+}
+
+function mostrarValour(){
+    
+    ocultar();
+    document.getElementById('valour').style.display="block";
+     document.getElementById('topHeader').style.display="block";
+    document.getElementById('navBar').style.display="block";
+    document.getElementById('footer').style.display="block";
+    document.getElementById('teams').style.display="block";
+}
+
+function ocultar(){
+    
+    document.getElementById('topHeader').style.display="none";
+    document.getElementById('navBar').style.display="none";
+    document.getElementById('scoreboard').style.display="none";
+    document.getElementById('sliderImg').style.display="none";
+    document.getElementById('schedule_home').style.display="none";
+    document.getElementById('footer').style.display="none";
+    document.getElementById('contact').style.display="none";
+    document.getElementById('u1').style.display="none";
+    document.getElementById('u2').style.display="none";
+    document.getElementById('u3').style.display="none";
+    document.getElementById('u4').style.display="none";
+    document.getElementById('u5').style.display="none";
+    document.getElementById('u6').style.display="none";
+    document.getElementById('calvary').style.display="none";
+    document.getElementById('york').style.display="none";
+    document.getElementById('edmonton').style.display="none";
+    document.getElementById('valour').style.display="none";
+    document.getElementById('pacific').style.display="none";
+    document.getElementById('forge').style.display="none";
+    document.getElementById('maps').style.display="none";
+    document.getElementById('teams').style.display="none";
+}
+
+
+
