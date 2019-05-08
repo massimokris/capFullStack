6e145dfedd52877a27cec276d8af3befe09f8566
massimokris <47436653+massimokris@users.noreply.github.com>
2019-05-08 07:15:00 -0300

    intentando

--- NYSLmobile/public/scripts/main.js
+++ NYSLmobile/public/scripts/main.js
@@ -1,29 +1,28 @@
 (function mostrarSchedule(){
-    
+
     ocultar();
     document.getElementById('topHeader').style.display="block";
     document.getElementById('navBar').style.display="block";
     document.getElementById('scoreboard').style.display="block";
     document.getElementById('sliderImg').style.display="block";
     document.getElementById('schedule_home').style.display="block";
-    
     document.getElementById('footer').style.display="block";
 })();
 
 function mostrarSchedule2(){
-    
+
     ocultar();
     document.getElementById('topHeader').style.display="block";
     document.getElementById('navBar').style.display="block";
     document.getElementById('scoreboard').style.display="block";
     document.getElementById('sliderImg').style.display="block";
     document.getElementById('schedule_home').style.display="block";
-    
+
     document.getElementById('footer').style.display="block";
 }
 
 function mostrarMaps(){
-    
+
     ocultar();
     document.getElementById('maps').style.display="block";
     document.getElementById('topHeader').style.display="block";
@@ -32,7 +31,7 @@ function mostrarMaps(){
 }
 
 function mostrarContact(){
-    
+
     ocultar();
     document.getElementById('contact').style.display="block";
      document.getElementById('topHeader').style.display="block";
@@ -41,7 +40,7 @@ function mostrarContact(){
 }
 
 function mostrarU1(){
-    
+
     ocultar();
     document.getElementById('u1').style.display="block";
      document.getElementById('topHeader').style.display="block";
@@ -50,7 +49,7 @@ function mostrarU1(){
 }
 
 function mostrarU2(){
-    
+
     ocultar();
     document.getElementById('u2').style.display="block";
      document.getElementById('topHeader').style.display="block";
@@ -59,7 +58,7 @@ function mostrarU2(){
 }
 
 function mostrarU3(){
-    
+
     ocultar();
     document.getElementById('u3').style.display="block";
      document.getElementById('topHeader').style.display="block";
@@ -68,7 +67,7 @@ function mostrarU3(){
 }
 
 function mostrarU4(){
-    
+
     ocultar();
     document.getElementById('u4').style.display="block";
      document.getElementById('topHeader').style.display="block";
@@ -77,7 +76,7 @@ function mostrarU4(){
 }
 
 function mostrarU5(){
-    
+
     ocultar();
     document.getElementById('u5').style.display="block";
      document.getElementById('topHeader').style.display="block";
@@ -86,7 +85,7 @@ function mostrarU5(){
 }
 
 function mostrarU6(){
-    
+
     ocultar();
     document.getElementById('u6').style.display="block";
      document.getElementById('topHeader').style.display="block";
@@ -95,7 +94,7 @@ function mostrarU6(){
 }
 
 function mostrarCalvary(){
-    
+
     ocultar();
     document.getElementById('calvary').style.display="block";
      document.getElementById('topHeader').style.display="block";
@@ -105,7 +104,7 @@ function mostrarCalvary(){
 }
 
 function mostrarEdmonton(){
-    
+
     ocultar();
     document.getElementById('edmonton').style.display="block";
      document.getElementById('topHeader').style.display="block";
@@ -115,7 +114,7 @@ function mostrarEdmonton(){
 }
 
 function mostrarForge(){
-    
+
     ocultar();
     document.getElementById('forge').style.display="block";
      document.getElementById('topHeader').style.display="block";
@@ -125,7 +124,7 @@ function mostrarForge(){
 }
 
 function mostrarPacific(){
-    
+
     ocultar();
     document.getElementById('pacific').style.display="block";
      document.getElementById('topHeader').style.display="block";
@@ -135,7 +134,7 @@ function mostrarPacific(){
 }
 
 function mostrarYork(){
-    
+
     ocultar();
     document.getElementById('york').style.display="block";
      document.getElementById('topHeader').style.display="block";
@@ -145,17 +144,17 @@ function mostrarYork(){
 }
 
 function mostrarValour(){
-    
+
     ocultar();
     document.getElementById('valour').style.display="block";
-     document.getElementById('topHeader').style.display="block";
+    document.getElementById('topHeader').style.display="block";
     document.getElementById('navBar').style.display="block";
     document.getElementById('footer').style.display="block";
     document.getElementById('teams').style.display="block";
 }
 
 function ocultar(){
-    
+
     document.getElementById('topHeader').style.display="none";
     document.getElementById('navBar').style.display="none";
     document.getElementById('scoreboard').style.display="none";
@@ -177,131 +176,221 @@ function ocultar(){
     document.getElementById('forge').style.display="none";
     document.getElementById('maps').style.display="none";
     document.getElementById('teams').style.display="none";
+    document.getElementById('fullMatches').style.display="none";
+}
+
+function fullMatch(){
+
+    ocultar();
+    document.getElementById('fullMatches').style.display="block";
+    document.getElementById('topHeader').style.display="block";
+    document.getElementById('navBar').style.display="block";
+    document.getElementById('footer').style.display="block";
+}
+
+function button(){
+
+    var x = document.getElementById("navNavbar").getAttribute("class");
+    var y = document.getElementById("categoriesMenu").getAttribute("class");
+    var z = document.getElementById("teamsMenu").getAttribute("class");
+
+    if(x == "navbar-collapse collapse show"){
+
+      x = "navbar-collapse collapse"
+    }
+
+    if(y == "collapse show"){
+
+      y = "collapse"
+    }
+
+    if(z == "collapse show"){
+
+      z = "collapse"
+    }
+
+    document.getElementById("navNavbar").setAttribute("class", x);
+    document.getElementById("categoriesMenu").setAttribute("class", x);
+    document.getElementById("teamsMenu").setAttribute("class", x);
+    //document.getElementById("buttonNavbar").innerHTML = "class =" + x;
 }
 
 //funcion para agregar categorias al dropdown
 function tableCategories(){
-    
+
     var category;
     var tableCategories;
-    
+
     category = data.categories;
-    
+
     console.log(category);
-    
+
     tableCategories = "<option id='category'>all</option>";
-    
+
     for(var i = 0; i < category.length; i++){
-        
+
         tableCategories += "<option id='category'>"+category[i]+"</option>";
     }
-    
+
     console.log(tableCategories);
-    
+
     return tableCategories;
 }
 
 if(document.getElementById("categories-data")){
-    
+
     //vinculamos esa funcion al html
-    document.getElementById("categories-data").innerHTML = tableCategories(); 
+    document.getElementById("categories-data").innerHTML = tableCategories();
 }
 
 //funcion para agregar fechas al dropdown
 function tableDate(){
-    
+
     var dates;
     var tableDate;
-    
+
     dates = data.allgames;
-    
+
     tableDate = "<option id='category'>all</option>";
-    
+
     for(var i = 0; i < dates.length; i++){
-        
+
         tableDate += "<option id='category'>"+dates[i].date+"</option>";
     }
-    
+
     return tableDate;
 }
 
 if(document.getElementById("date")){
-    
+
     //vinculamos esa funcion al html
-    document.getElementById("date").innerHTML = tableDate(); 
+    document.getElementById("date").innerHTML = tableDate();
 }
 
 function matchesList(){
-    
+
     var listCategories;
     var listDates;
     var list;
     var printList = "";
     var maps;
     var place;
-    
+
     listCategories = document.getElementById("categories-data").value;
     listDates = document.getElementById("date").value;
-    
+
     console.log(document.getElementById("categories-data").value);
     console.log(document.getElementById("date").value);
-    
-    list = data.allgames;
-    
-    console.log(list[0].games.length);
-    
-    for(var i = 0; i < list.length; i++){
-        
-        if(list[i].date == listDates || listDates == 'all'){
-            
-            //printList += "<li>"+"<b>"+list[i].date+"</b>"   
-        }
-        
-            for(var j = 0; j < list[i].games.length; j++){
-                
-                if((list[i].games[j].categories[j] == listCategories || listCategories == 'all') && (list[i].date == listDates || listDates == 'all')){
-                    
-                    printList += "<li>"+"<b>"+list[i].date+"</b>"+
-                        
-                    "<ul>"+"<li>"+list[i].games[j].categories[j]+"</li>"+
-                
-                    "<li>"+"<a href='#' onclick='showMap("list[i].games[j].place")'>"+list[i].games[j].place+"</a"+"</li>"+
 
-                    "<li>"+list[i].games[j].time+"</li>"+"</ul>"    
+    list = data.allgames;
+
+    console.log(list[0].games.length);
+
+    for(var i = 0; i < list.length; i++){
+
+        if(list[i].date == listDates || listDates == 'all'){
+
+            //printList += "<li>"+"<b>"+list[i].date+"</b>"
+        }
+
+            for(var j = 0; j < list[i].games.length; j++){
+
+                if((list[i].games[j].categories[j] == listCategories || listCategories == 'all') && (list[i].date == listDates || listDates == 'all')){
+
+                    printList += "<li>"+"<b>"+list[i].date+"</b>"+
+
+                    "<ul>"+"<li>"+list[i].games[j].categories[j]+"</li>"+
+
+                    '<li><a onclick=showMap("'+list[i].games[j].place+'");>'+list[i].games[j].place+"</a></li>"+
+
+                    "<li>"+list[i].games[j].time+"</li>"+"</ul>"
                 }
             }
-        
+
             "</li>";
     }
-    
-    
+
+
     return printList;
 }
 
 if(document.getElementById("schedule_info")){
-    
+
     //vinculamos esa funcion al html
-    document.getElementById("schedule_info").innerHTML = matchesList(); 
+    document.getElementById("schedule_info").innerHTML = matchesList();
 }
 
 if(document.getElementById("schedule_info")){
-    /*cambiamos la tabla en base a el estado que elige el usuario*/ 
+    /*cambiamos la tabla en base a el estado que elige el usuario*/
       document.getElementById("date").addEventListener("change", function(){
         document.getElementById("schedule_info").innerHTML = matchesList();
-    });  
+    });
 }
 
 if(document.getElementById("schedule_info")){
-    /*cambiamos la tabla en base a el estado que elige el usuario*/ 
+    /*cambiamos la tabla en base a el estado que elige el usuario*/
       document.getElementById("categories-data").addEventListener("change", function(){
         document.getElementById("schedule_info").innerHTML = matchesList();
-    });  
+    });
 }
 
 function showMap(string){
-    
+
     switch(string){
-            
-        //pasar case de mapas para cambiar el display a block
+
+        case "AJ-Katzenmaier":
+          document.getElementById('katzen').style.display="block";
+          document.getElementById('green').style.display="none";
+          document.getElementById('howard').style.display="none";
+          document.getElementById('north').style.display="none";
+          document.getElementById('hart').style.display="none";
+          document.getElementById('south').style.display="none";
+        break;
+
+        case "Greenbay":
+          document.getElementById('green').style.display="block";
+          document.getElementById('katzen').style.display="none";
+          document.getElementById('howard').style.display="none";
+          document.getElementById('north').style.display="none";
+          document.getElementById('hart').style.display="none";
+          document.getElementById('south').style.display="none";
+        break;
+
+        case "Howard-Yeager":
+          document.getElementById('howard').style.display="block";
+          document.getElementById('green').style.display="none";
+          document.getElementById('katzen').style.display="none";
+          document.getElementById('north').style.display="none";
+          document.getElementById('hart').style.display="none";
+          document.getElementById('south').style.display="none";
+        break;
+
+        case "Marjorie-Hart":
+          document.getElementById('hart').style.display="block";
+          document.getElementById('green').style.display="none";
+          document.getElementById('howard').style.display="none";
+          document.getElementById('north').style.display="none";
+          document.getElementById('katzen').style.display="none";
+          document.getElementById('south').style.display="none";
+        break;
+
+        case "North":
+          document.getElementById('north').style.display="block";
+          document.getElementById('green').style.display="none";
+          document.getElementById('howard').style.display="none";
+          document.getElementById('hart').style.display="none";
+          document.getElementById('katzen').style.display="none";
+          document.getElementById('south').style.display="none";
+        break;
+
+        case "South":
+          document.getElementById('south').style.display="block";
+          document.getElementById('green').style.display="none";
+          document.getElementById('howard').style.display="none";
+          document.getElementById('north').style.display="none";
+          document.getElementById('hart').style.display="none";
+          document.getElementById('katzen').style.display="none";
+        break;
     }
+
 }
